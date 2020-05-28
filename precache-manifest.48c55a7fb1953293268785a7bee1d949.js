4ae1a54679a39b7b93b560db162239ae6b137f2f
Marten <marten.frisk@gmail.com>
2020-05-28 15:05:25 +0200

    Updates

---
+++ precache-manifest.48c55a7fb1953293268785a7bee1d949.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "035e2df7ac30d4266e69bd7ac455f817",
+    "url": "/seekerslounge/index.html"
+  },
+  {
+    "revision": "5e5eea219cd1315e9221",
+    "url": "/seekerslounge/static/css/main.bd69dbd8.chunk.css"
+  },
+  {
+    "revision": "2db2287aa5b7a77f1483",
+    "url": "/seekerslounge/static/js/2.d04d740b.chunk.js"
+  },
+  {
+    "revision": "b47b4efa270fc30d7c6dd21ea9e429ad",
+    "url": "/seekerslounge/static/js/2.d04d740b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e5eea219cd1315e9221",
+    "url": "/seekerslounge/static/js/main.30acfca9.chunk.js"
+  },
+  {
+    "revision": "2ea3f4e18bdfffcc9df8",
+    "url": "/seekerslounge/static/js/runtime-main.9f4d1a39.js"
+  },
+  {
+    "revision": "3badc09577faa0f4fcacafd1c07514e2",
+    "url": "/seekerslounge/static/media/coffee.3badc095.svg"
+  }
+]);
