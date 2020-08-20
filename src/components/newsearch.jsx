6a5bbef58f31f56dd22dac8ce47696688dde3f3c
Marten <marten.frisk@gmail.com>
2020-08-20 20:59:06 +0200

    Add season filter

--- src/components/newsearch.jsx
+++ src/components/newsearch.jsx
@@ -6,6 +6,7 @@ import AutosuggestHighlightMatch from 'autosuggest-highlight/match'
 import AutosuggestHighlightParse from 'autosuggest-highlight/parse'
 import epList from '../assets/episodes.json'
 import { Link, withRouter } from 'react-router-dom'
+import qs from 'qs'
 
 const randomQuery = [
 	'guinness',
@@ -29,32 +30,45 @@ const randomQuery = [
 	'goths',
 	'famously',
 	'oj simpson',
-	'let\'s just say'
+	"let's just say"
 ]
+
 function getRandomInt(max) {
 	return Math.floor(Math.random() * Math.floor(max))
 }
 
-const Search = () => {
-	const [ results, setResults ] = useState()
+let seasonArr = [ 's01', 's02', 's03', 's04', 's05', 's06', 's07', 's08', 's09', 'min' ]
+
+const Search = ({ history }) => {
+	const [ results, setResults ] = useState([])
 	const [ searchValue, setSearchValue ] = useState(randomQuery[getRandomInt(randomQuery.length)])
 	const [ searchDetails, setSearchDetails ] = useState()
+	const [ exact, setExact ] = useState(false)
+	const [ usedExact, setUsedExact ] = useState(false)
+	const [ filterSeason, setFilterSeason ] = useState(
+		qs.parse(history.location.search)['s'] !== undefined
+			? qs.parse(history.location.search)['s'].split(',')
+			: [ 'all' ]
+	)
 
-  useEffect(() => {
-    onSuggestionFetchFuzzy()
-  }, [])
-  
-	const renderSuggestion = (suggestion, query, index) => {
+	const goSearch = () => {
+		exact ? searchExact() : searchFuzzy()
+	}
+
+	useEffect(() => {
+		goSearch()
+	}, [])
+
+	const renderSuggestion = (suggestion, index) => {
 		const suggestionText = suggestion.line
-		const matches = AutosuggestHighlightMatch(suggestionText, query)
+		const matches = AutosuggestHighlightMatch(suggestionText, searchValue)
 		const parts = AutosuggestHighlightParse(suggestionText, matches)
 		let epClean = suggestion.episode.replace('.json', '')
 		let epName = epList.find((x) => x.ep === epClean)
 
-
 		return (
 			<LazyLoad key={index}>
-				<div className="min-w-full px-4 pb-6 mb-6 shadow-md md:mx-4">
+				<div className="w-full px-4 pb-6 mb-6 shadow-md">
 					<div className="flex flex-wrap items-center justify-between w-full mb-2">
 						<div className="flex items-center">
 							<div className="pt-1 mr-2 text-xs text-gray-700 uppercase">{epName.ep}</div>
@@ -96,7 +110,7 @@ const Search = () => {
 		)
 	}
 
-	const onSuggestionFetchFuzzy = async () => {
+	const searchFuzzy = async () => {
 		await axios
 			.post('https://search-seekerslounge-bfv6hl5b7dikv4ehjzd3gs4tsu.us-east-1.es.amazonaws.com/teach/_search', {
 				from: 0,
@@ -115,12 +129,45 @@ const Search = () => {
 				setResults(() => results)
 				setSearchDetails(() => res.data.hits.total.value)
 				console.log(results)
+				setUsedExact(false)
 			})
-  }
-  
+	}
+
+	const searchExact = async () => {
+		await axios
+			.post('https://search-seekerslounge-bfv6hl5b7dikv4ehjzd3gs4tsu.us-east-1.es.amazonaws.com/teach/_search', {
+				from: 0,
+				size: 99,
+				query: {
+					multi_match: {
+						query: searchValue,
+						fields: [ 'line', 'episode' ],
+						type: 'phrase',
+						operator: 'and'
+					}
+				}
+			})
+			.then((res) => {
+				const results = res.data.hits.hits.map((h) => h._source)
+				setResults(() => results)
+				setSearchDetails(() => res.data.hits.total.value)
+				setUsedExact(true)
+			})
+	}
+
+	const handleSeasonFilter = (season) => {
+		if (filterSeason.includes('all')) {
+			setFilterSeason(() => [ season ])
+		} else if (!filterSeason.includes(season)) {
+			setFilterSeason((filterSeason) => [ ...filterSeason, season ])
+		} else {
+			setFilterSeason((filterSeason) => filterSeason.filter((p) => p !== season))
+		}
+	}
+
 	const handleSubmit = (e) => {
 		e.preventDefault()
-		onSuggestionFetchFuzzy()
+		exact ? searchExact() : searchFuzzy()
 	}
 
 	const handleChange = (e) => {
@@ -128,21 +175,102 @@ const Search = () => {
 		setSearchValue(e.target.value)
 	}
 
+	const handleCheckbox = () => {
+		setExact((prev) => !prev)
+		goSearch()
+	}
+
+	const sortSuggestions = (suggestions, index) => {
+		if (suggestions) {
+			if (filterSeason.includes('all')) {
+				return renderSuggestion(suggestions, index)
+			} else if (filterSeason.length === 0) {
+				setFilterSeason([ 'all' ])
+				return renderSuggestion(suggestions, index)
+			} else {
+				let currentSeason = suggestions.episode.slice(0, 3)
+				if (filterSeason.includes(currentSeason)) {
+					return renderSuggestion(suggestions, index)
+				}
+			}
+		} else {
+			return []
+		}
+	}
+
 	return (
-		<>
-			<form onSubmit={handleSubmit}>
+		<React.Fragment>
+			<form className="flex flex-wrap items-center mb-4 md:m-4" onSubmit={handleSubmit}>
+				<span className="inline mr-1 text-sm">
+					Search exact matches (click 'Search' after ticking box):&nbsp;
+					<input id="check" type="checkbox" className="inline" onChange={handleCheckbox} checked={exact} />
+				</span>
+
 				<input
 					onChange={handleChange}
 					type="text"
-					className="w-full h-8 mx-4 border border-gray-500"
+					className="w-full h-12 px-4 text-xl border border-gray-500 md:w-4/6"
 					name="search"
+					autoFocus
 					value={searchValue}
 				/>
-				<input type="submit" value="Search" />
+				<button
+					onClick={() => setSearchValue(randomQuery[getRandomInt(randomQuery.length)])}
+					className="w-1/2 h-10 text-white bg-blue-400 border-r border-white md:h-12 md:w-1/6"
+				>
+					Random
+				</button>
+				<input type="submit" className="w-1/2 h-10 text-white bg-blue-400 md:h-12 md:w-1/6" value="Search" />
 			</form>
-			<div>{results && results.map((hit, index) => renderSuggestion(hit, searchValue, index) )}</div>
-			<p>Search value: {searchValue}</p>
-		</>
+			<div className="px-2 md:px-8">
+				Filter by season:&nbsp;
+				{filterSeason.includes('all') ? (
+					<button className="px-1 mr-2 bg-blue-300 rounded-sm" onClick={() => setFilterSeason([ 'all' ])}>
+						all
+					</button>
+				) : (
+					<button className="px-1 mr-2" onClick={() => setFilterSeason([ 'all' ])}>
+						all
+					</button>
+				)}
+				{seasonArr.map((season) => {
+					if (filterSeason.includes(season)) {
+						return (
+							<button
+								className="px-1 mr-2 bg-blue-300 rounded-sm"
+								onClick={() => handleSeasonFilter(season)}
+								key={season}
+							>
+								{season === 'min' ? 'mini' : season}
+							</button>
+						)
+					} else {
+						return (
+							<button className="px-1 mr-2" onClick={() => handleSeasonFilter(season)} key={season}>
+								{season === 'min' ? 'mini' : season}
+							</button>
+						)
+					}
+				})}
+			</div>
+			<div className="md:px-4">
+				<div className="px-4 my-4">
+					{searchDetails !== 0 &&
+						`${searchDetails} hits for "${searchValue}" using ${usedExact
+							? 'exact'
+							: 'fuzzy'} search in ${filterSeason.includes('all') ? 'all seasons' : filterSeason.map(s => s === 'min' ? ' mini' : ` ${s}`)}`}
+				</div>
+
+				{results.length !== 0 ? (
+					results.map((hit, index) => sortSuggestions(hit, index))
+				) : (
+					<p>
+						No results for "{searchValue}" in {filterSeason.includes('all') ? 'all seasons' : filterSeason}.
+						Try searching phonetically or inserting spaces in the words.
+					</p>
+				)}
+			</div>
+		</React.Fragment>
 	)
 }
 
